2feff8ccc02258bf8af0568f37a99feea729e116
+ <+>
2022-11-08 16:35:57 +0300

    updated the the todo migration

--- db/migrate/20221108133159_create_Todos.rb
+++ db/migrate/20221108133159_create_Todos.rb
@@ -1,4 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
-  def change
-  end
+    def change
+      create_table :todos do |t|
+        t.string :item
+        t.string :importance
+        t.integer :category_id
+      end
+    end
 end
