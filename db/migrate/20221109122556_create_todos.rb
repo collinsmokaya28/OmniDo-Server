0002758679085bf729142151d9c9d004ffbbb591
Felix <barosiofelix@gmail.com>
2022-11-09 15:29:16 +0300

    Added todo values to file

--- db/migrate/20221109122556_create_todos.rb
+++ db/migrate/20221109122556_create_todos.rb
@@ -1,4 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
+    t.string :text
+    t.string :category
+    t.boolean :done
+    t.string :user_id
+
+    t.timestamps null: false
   end
 end
