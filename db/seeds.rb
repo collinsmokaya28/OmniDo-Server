a446c51df9b0473d52ca64dc6d0f9e9ff853d52f
Felix <barosiofelix@gmail.com>
2022-11-09 17:09:08 +0300

    Added data for users and todos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
 puts "🌱 Seeding spices..."
 
+
+puts "Destroying old data ,,,,,,,,,,,"
+User.destroy_all
+Todo.destroy_all
+
 # Seed your database here
 
+puts "Seed users,,,,,,,,..."
 
+user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+)
+
+todo = Todo.create(
+    text: "Coding some stuff",
+    category: "Coding",
+    done: false,
+    user_id: user.id
+)
 
 puts "✅ Done seeding!"
