e8ed326cca8061a1a5a75c6c9c103c03a9736c13
Felix <barosiofelix@gmail.com>
2022-11-09 15:34:43 +0300

    Added get to fetch all todos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
+  end
 
+  get '/todos' do
+    todo = Todo.all
+    todo.to_json
+  end
 
 end
