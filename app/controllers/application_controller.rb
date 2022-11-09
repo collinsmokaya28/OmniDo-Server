73f1d9a8e40635d0d21e762ed340c8e2a5753d43
Felix <barosiofelix@gmail.com>
2022-11-09 15:13:03 +0300

    Deleted Data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/categories" do
-    categories = Category.all
-    categories.to_json
-  end
+
 
 end
