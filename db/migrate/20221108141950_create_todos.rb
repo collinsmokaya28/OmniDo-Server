1d8b817b2daa64f3358466e923101727be68d041
Felix <barosiofelix@gmail.com>
2022-11-09 15:11:22 +0300

    Deleted Data

--- db/migrate/20221108141950_create_todos.rb
+++ db/migrate/20221108141950_create_todos.rb
@@ -1,9 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
-      t.string :item
-      t.string :importance
-      t.integer :category_id
     end
   end
 end
